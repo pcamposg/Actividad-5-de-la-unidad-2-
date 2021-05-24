56b6b05b035494de56686a048597db61b41a14e9
pcamposg <pcamposg@ibero.edu.co>
2021-05-23 19:00:32 -0500

    Mi primer repositorio

---
+++ js/jquery.js
@@ -0,0 +1,20 @@
+
+$('.opcion1').mouseover(function(){
+    $(this).css('font-size','40px')
+})
+
+$('.opcion2').mouseover(function(){
+    $(this).css('font-size','40px')
+})
+
+$('.opcion3').mouseover(function(){
+    $(this).css('font-size','40px')
+})
+
+$('.opcion4').mouseover(function(){
+    $(this).css('font-size','40px')
+})
+
+$('.opcion5').mouseover(function(){
+    $(this).css('font-size','40px')
+})
